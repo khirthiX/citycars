dc87252709fc4ceb2cb811e1119e16da8063dec3
khirthix <khirthi.xendworks@gmail.com>
2024-11-05 04:11:46 +0530

    modified nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ import { defineNuxtConfig } from "nuxt/config";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false, // Enable SSR
-  nitro: {
-    preset: 'firebase', // or 'vercel', 'netlify', etc. based on your hosting provider
-  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   css: ["@/assets/styles/main.scss"],
