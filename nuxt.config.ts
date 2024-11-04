847aa40b3cdfb4f4a98811580d7aa25a0d4c968a
khirthix <khirthi.xendworks@gmail.com>
2024-11-05 02:34:31 +0530

    changed nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,13 @@ import { defineNuxtConfig } from "nuxt/config";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: true, // Enable SSR
   compatibilityDate: '2024-04-03',
   devtools: { enabled: false },
   css: ["@/assets/styles/main.scss"],
   plugins: [
     { src: '~/plugins/chat.ts', mode: 'client' } 
   ],
-  modules: ["@nuxtjs/i18n", '@pinia/nuxt',]
+  modules: ["@nuxtjs/i18n", '@pinia/nuxt',],
+  
 })
